16f39bc47e3cbdaf370928f104f0b1c5da870c68
Pierre Tanguay <luctanguay101@gmail.com>
2023-02-23 20:29:55 -0500

    Added gen_server hello_world.

--- src/hello_world_sup.erl
+++ src/hello_world_sup.erl
@@ -29,7 +29,8 @@ init([]) ->
     SupFlags = #{strategy => one_for_all,
                  intensity => 0,
                  period => 1},
-    ChildSpecs = [],
+    ChildSpecs = [#{id => hello_world,
+		    start => {hello_world, start_link, []}}],
     {ok, {SupFlags, ChildSpecs}}.
 
 %% internal functions
